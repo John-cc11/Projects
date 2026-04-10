c74fd2c08a9d30cbd2103d0bd380d6696581b35b
John-cc11 <johnashleycardino@gmail.com>
2026-04-10 16:16:33 +0800

    firstproject

--- firstproject/maingui.py
+++ firstproject/maingui.py
@@ -14,20 +14,28 @@ class App(ctk.CTk):
         self.geometry("1000x600")  
         self.minsize(800, 500)
 
-       
+     
         self.resizable(True, True)
 
      
         self.main_frame = ctk.CTkFrame(self, corner_radius=0)
         self.main_frame.pack(fill="both", expand=True)
 
-        
-        self.label = ctk.CTkLabel(
-            self.main_frame,
-            text="Welcome to Library System",
-            font=("Arial", 24)
-        )
-        self.label.pack(pady=40)
+        #====================== sidebar ============
+        self.sidebar = ctk.CTkFrame(self.main_frame, width=200, corner_radius=0)
+        self.sidebar.pack(side = "left", fill = "y")
+
+        #====================== Content  ============
+
+        self.content = ctk.CTkFrame(self.main_frame, corner_radius= 0)
+        self.content.pack(side ="left", fill="both", expand=True)
+
+        self.side_label = ctk.CTkLabel(self.sidebar, text="MENU")
+        self.side_label.pack(pady=20)
+
+        self.main_label = ctk.CTkLabel(self.content, text="Dashboard", font=("Arial", 24))
+        self.main_label.pack(pady=40)
+       
 
 
 
