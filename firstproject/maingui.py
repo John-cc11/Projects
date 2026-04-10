de6dfda281fb8dac56111026702b105a63182ecb
John-cc11 <johnashleycardino@gmail.com>
2026-04-10 23:32:51 +0800

    firstproject

--- firstproject/maingui.py
+++ firstproject/maingui.py
@@ -24,6 +24,16 @@ class App(ctk.CTk):
         #====================== sidebar ============
         self.sidebar = ctk.CTkFrame(self.main_frame, width=200, corner_radius=0)
         self.sidebar.pack(side = "left", fill = "y")
+        
+        self.dashboard_btn  = ctk.CTkButton(self.sidebar, text="Dashboard", width=180)
+        self.books_btn  = ctk.CTkButton(self.sidebar, text="Books", width=180)
+        self.users_btn  = ctk.CTkButton(self.sidebar, text="Users", width=180)
+        self.borrowing_btn  = ctk.CTkButton(self.sidebar, text="Borrowing", width=180)
+        self.records_btn  = ctk.CTkButton(self.sidebar, text="Reports", width=180)
+        self.settings_btn  = ctk.CTkButton(self.sidebar, text="Setting", width=180)
+        self.sign_out_btn = ctk.CTkButton(self.sidebar, text="Sign Out", width=180)
+
+
 
         #====================== Content  ============
 
@@ -32,6 +42,16 @@ class App(ctk.CTk):
 
         self.side_label = ctk.CTkLabel(self.sidebar, text="MENU")
         self.side_label.pack(pady=20)
+
+
+        self.dashboard_btn.pack(pady=10, padx=10)
+        self.books_btn.pack(pady=10, padx=10)
+        self.users_btn.pack(pady=10, padx=10)
+        self.borrowing_btn.pack(pady=10, padx=10)
+        self.records_btn.pack(pady=10, padx=10)
+        self.settings_btn.pack(pady=10, padx=10)
+        self.sign_out_btn.pack(pady=10, padx=10)
+
 
         self.main_label = ctk.CTkLabel(self.content, text="Dashboard", font=("Arial", 24))
         self.main_label.pack(pady=40)
